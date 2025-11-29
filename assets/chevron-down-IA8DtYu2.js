c3eb1bc2b1c64457996900385253af19219e5b0e
inwhitemist <gregory_05@bk.ru>
2025-11-29 13:59:51 +0300

    Updates

--- assets/chevron-down-IA8DtYu2.js
+++ assets/chevron-down-IA8DtYu2.js
@@ -1,4 +1,4 @@
-import{c as o}from"./index-DpYuSFO7.js";/**
+import{c as o}from"./index-EbsA_0Ao.js";/**
  * @license lucide-react v0.453.0 - ISC
  *
  * This source code is licensed under the ISC license.
